7b6a231e52b0cd1cda6f8453bd58af9a838909bf
antonymburia <antonymburia10@gmail.com>
2021-12-03 15:38:12 +0300

    add js functionality to print out skills

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 const text = ['Front end developer no', 'Backend dev sorry', 'Full Stack Developer'];
+let count = 0;
+index = 0;
+let currenttxt = '';
+let character = '';
+
+(function typing () {
+  if (count === text.length) {
+    count = 0;
+  }
+  currenttxt = text[count];
+  character = currenttxt.slice(0, ++index);
+
+  document.querySelector('.screen-type').textContent = character;
+  if (character.length === currenttxt.length) {
+    count++;
+    index = 0;
+  }
+  setTimeout(typing, 300);
+}());
